7707f66369432e892046588f1f7496699b027cde
timelyportfolio <kent.russell@timelyportfolio.com>
2015-03-03 12:02:51 -0600

    set display = none for now

--- inst/htmlwidgets/sortableR.js
+++ inst/htmlwidgets/sortableR.js
@@ -6,11 +6,19 @@ HTMLWidgets.widget({
 
   initialize: function(el, width, height) {
 
+    // for now set display to none if height and width = 0
+    //  however eventually might add option to wrap
+    //  a list or vector within sortableR
+    if(width === 0 && height === 0){
+      el.style.display = "none"
+    }
+
     return { };
 
   },
 
   renderValue: function(el, x, instance) {
+
 
     instance.sortable = Sortable.create(
       document.getElementById( x.selector ),
