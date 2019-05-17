449fe72c0ad8a4c6ee90bbf64355f2161ea41116
Andrie de Vries <apdevries@gmail.com>
2019-05-17 18:30:20 +0100

    Improve `sortableItemlist()` - you no longer have to supply a `selector`; Added example to Readme #7

--- R/sortableItemlist.R
+++ R/sortableItemlist.R
@@ -1,11 +1,13 @@
+# Construct the onSort method for sortable
 sortableItemlist_onsort <- function(outputId){
   innerText <- '
     $.map(this.el.children, function(child){return child.innerText})
   '
-  js <- glue::glue('function(evt){{
-    debugger
-    Shiny.onInputChange("{outputId}", {innerText})
-  }}')
+  jsText <- 'function(evt){{
+    Shiny.onInputChange("%s", %s)
+  }}'
+
+  js <- sprintf(jsText, outputId, innerText)
 
   list(onSort = htmlwidgets::JS(js))
 }
@@ -13,13 +15,28 @@ sortableItemlist_onsort <- function(outputId){
 
 #' Sortable item list.
 #'
-#' @param outputId
-#' @param labels
-#' @param selector
-#' @param class
+#' Creates a sortable item list using the `sortable.js` framework, and generates
+#' an **htmlwidgets** element.
+#'
+#' @inheritParams sortable
+#'
+#' @param outputId output variable to read the plot/image from.
+#' @param labels A character vector with text to display.
+#' @param selector This is the css id to use, and must be unique in your shiny
+#'   app. If NULL, the function generates a selector of the form
+#'   `sortable_itemlist_id_1`, and will automatically increment for every
+#'   `sortableItemlist`
+#' @param class The css class to use
+#'
+#' @seealso [sortable]
 #'
 #' @export
+#' @importFrom htmltools tags
 sortableItemlist <- function(outputId, labels, selector, class = "list-group-item"){
+  if (missing(selector) || is.null(selector) || is.na(selector)) {
+    selector <- incrementSortableItemlist()
+  }
+  # browser()
   list(
     tags$div(
       id = selector,
