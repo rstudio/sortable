615ece5ddafdf8259250bd6e73190689d08a91ad
Barret Schloerke <schloerke@gmail.com>
2019-06-07 10:40:39 -0400

    add learnr methods and tests

---
+++ tests/testthat/test-learnr.R
@@ -0,0 +1,58 @@
+context("learnr")
+
+
+
+test_that( "init display validates", {
+
+  question <- learnr::question(
+    "Sort the first 5 letters",
+    learnr::answer(LETTERS[1:5], correct = TRUE),
+    learnr::answer(rev(LETTERS[1:5]), correct = FALSE, "Other direction!"),
+    type = "sortable"
+  )
+  expect_s3_class(question, "sortable")
+
+  expect_silent({
+    question_initialize_input(question, "ignored")
+  })
+
+  expect_error(
+    question_initialize_input(
+      learnr::question(
+        "Sort the first 5 letters",
+        learnr::answer(LETTERS[1:5], correct = TRUE),
+        learnr::answer(letters[1:5], correct = FALSE, "causes error"),
+        learnr::answer(rev(LETTERS[1:5]), correct = FALSE, "Other direction!"),
+        type = "sortable"
+      )
+    ),
+    "answers MUST have the same set"
+  )
+
+
+  expect_silent({
+    question_completed_input(question, LETTERS[5:1])
+  })
+
+  expect_true(
+    question_is_valid(question, letters[1:5])
+  )
+  expect_false(
+    question_is_valid(question, NULL)
+  )
+
+  expect_identical(
+    question_is_correct(question, LETTERS[1:5]),
+    learnr::question_is_correct_value(TRUE, NULL)
+  )
+  tmp_answer <- learnr::answer("ignored", FALSE, "Other direction!")
+  expect_identical(
+    question_is_correct(question, LETTERS[5:1]),
+    learnr::question_is_correct_value(FALSE, tmp_answer$message)
+  )
+  expect_identical(
+    question_is_correct(question, letters[1:5]),
+    learnr::question_is_correct_value(FALSE, NULL)
+  )
+
+})
