ba3c0d6c33f2fafadd16742c0315edfbd56644d4
Barret Schloerke <schloerke@gmail.com>
2019-06-07 10:38:24 -0400

    do not open webpage in test

--- tests/testthat/test-htmltools.R
+++ tests/testthat/test-htmltools.R
@@ -2,17 +2,19 @@ context("htmltools")
 
 library(htmltools)
 test_that( "works with tags ", {
-  z <- html_print(tagList(
-    tags$h1( "Check to make sure items move")
-    ,HTML('
-      <ul id="items">
-        <li>item 1</li>
-        <li>item 2</li>
-        <li>item 3</li>
-      </ul>
-    ')
-    ,sortable( "items" )
-  ))
+  z <- html_print(
+    viewer = NULL,
+    tagList(
+      tags$h1( "Check to make sure items move")
+      ,HTML('
+        <ul id="items">
+          <li>item 1</li>
+          <li>item 2</li>
+          <li>item 3</li>
+        </ul>
+      ')
+      ,sortable( "items" )
+    )
+  )
   expect_is(z, "character")
 })
-
