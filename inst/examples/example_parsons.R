0aaad064ae77b5206c5d2f7f184357331c32aabc
Andrie <apdevries@gmail.com>
2019-06-25 14:00:56 +0100

    Simplify arguments for `parsons()` #12

--- inst/examples/example_parsons.R
+++ inst/examples/example_parsons.R
@@ -5,17 +5,13 @@
 parsons(
   header = "This is an example of a Parsons problem",
   text = c("Drag from here", "Construct your solution here"),
-  labels = list(
-    c(
+  labels = c(
       "iris",
       "mutate(...)",
       "summarize(...)",
       "print()"
-    ),
-    NULL
   ),
-  selector = paste0("input", 1:2),
-  input_id = paste0("output", 1:2),
-  group_name = "parsons_test",
+  input_id = "input_parsons",
+  group_name = "parsons_test"
 )
 
