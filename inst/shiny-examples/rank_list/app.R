c26603281836b2d5f7072816aa7c72ae5d90d63a
Barret Schloerke <schloerke@gmail.com>
2019-08-15 10:54:44 -0400

    complicate the example app

--- inst/shiny-examples/rank_list/app.R
+++ inst/shiny-examples/rank_list/app.R
@@ -11,7 +11,25 @@ ui <- fluidPage(
       tags$b("Exercise"),
       rank_list(
         text = "Drag the items in the correct order",
-        labels = sample(c("one", "two", "three", "four", "five")),
+        labels = list(
+          "one",
+          "two",
+          "three",
+          htmltools::tags$div(
+            htmltools::tags$div(
+              id = "inner_element_4",
+              "Complex html tag(s) with no 'id' attribute in outer element."
+            )
+          ),
+          htmltools::tags$div(
+            id = "five",
+            "Extra text",
+            htmltools::tags$div(
+              id = "inner_element_5",
+              "Complex html tag(s) with id: 'five'"
+            )
+          )
+        ),
         input_id = "rank_list_1"
       ),
       tags$b("Result"),
