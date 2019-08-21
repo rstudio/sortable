544aeb72a1465256ec09a775c5f089c058ae94f7
Barret Schloerke <schloerke@gmail.com>
2019-08-21 10:37:54 -0400

    do not poison global env

--- scripts/compile_css.R
+++ scripts/compile_css.R
@@ -1,25 +1,27 @@
 
-if (!require(sass)) {
-  install.packages("sass")
-}
-library(sass)
-
-
-scss_files <- dir(
-  file.path("inst", "htmlwidgets", "plugins", "sortable-rstudio"),
-  pattern = "\\.scss",
-  full.names = TRUE
-)
-
-for (scss_file in scss_files) {
-  if (grepl("^_", basename(scss_file))) {
-    message("Skipping   : ", basename(scss_file))
-    NULL
-  } else {
-    message("Translating: ", basename(scss_file))
-    sass::sass(
-      input = sass::sass_file(scss_file),
-      output = sub("\\.scss", ".css", scss_file)
-    )
+local({
+  if (!require(sass)) {
+    install.packages("sass")
   }
-}
+  library(sass)
+
+
+  scss_files <- dir(
+    file.path("inst", "htmlwidgets", "plugins", "sortable-rstudio"),
+    pattern = "\\.scss",
+    full.names = TRUE
+  )
+
+  for (scss_file in scss_files) {
+    if (grepl("^_", basename(scss_file))) {
+      message("Skipping   : ", basename(scss_file))
+      NULL
+    } else {
+      message("Translating: ", basename(scss_file))
+      sass::sass(
+        input = sass::sass_file(scss_file),
+        output = sub("\\.scss", ".css", scss_file)
+      )
+    }
+  }
+})
