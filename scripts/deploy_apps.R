8301f4556499d59708fe4c0ad1f8cb2a72180637
Andrie <apdevries@gmail.com>
2020-02-05 11:06:38 +0000

    Deploy apps only if code is newer than shinyapps.io

--- scripts/deploy_apps.R
+++ scripts/deploy_apps.R
@@ -3,10 +3,15 @@ if (!requireNamespace("remotes")) install.packages("remotes")
 
 # install rsconnect and glue
 if (!requireNamespace("rsconnect")) remotes::install_cran("rsconnect")
-if (!requireNamespace("glue")) remotes::install_cran("glue")
+if (!requireNamespace("glue"))      remotes::install_cran("glue")
+if (!requireNamespace("rsconnect")) remotes::install_cran("rsconnect")
 
-# install the latest
-remotes::install_github("rstudio/sortable")
+
+# install the latest version when running on travis
+
+if (Sys.getenv("TRAVIS") == "true") {
+  remotes::install_github("rstudio/sortable")
+}
 
 
 
@@ -15,17 +20,30 @@ deploy_app <- function(
   name = glue::glue("sortable_{basename(app_dir)}_app"),
   ...
 ) {
-  cat("\n\n\n")
-  message("Deploying: ", name)
-  cat("\n")
-  rsconnect::deployApp(
-    appDir = app_dir,
-    appName = name,
-    server = "shinyapps.io",
-    account = "andrie-de-vries",
-    forceUpdate = TRUE,
-    ...
-  )
+  server  <- "shinyapps.io"
+  account <- "andrie-de-vries"
+
+  all_apps <- rsconnect::applications(account = account, server = server)
+  last_deployed <- all_apps[all_apps[["name"]] == name, "updated_time"]
+  last_updated <- max(file.mtime(list.files(app_dir, recursive = TRUE, full.names = TRUE)))
+
+  last_deployed <- as.POSIXct(last_deployed, format = "%FT%T")
+
+  if (!length(last_deployed) || last_updated > last_deployed) {
+    message("\n\n\n")
+    message("Deploying: ", name)
+    message("\n")
+    rsconnect::deployApp(
+      appDir = app_dir,
+      appName = name,
+      server = server,
+      account = account,
+      forceUpdate = TRUE,
+      ...
+    )
+  } else {
+    message("Nothing to do for ", name)
+  }
 }
 
 deploy_tutorial <- function(
@@ -42,14 +60,14 @@ deploy_tutorial <- function(
 
 
 deploy_folder <- function(path, fn) {
-  lapply(
+  invisible(lapply(
     dir(path, full.names = TRUE),
     function(path) {
       if (dir.exists(path)) {
         fn(path)
       }
     }
-  )
+  ))
 }
 
 deploy_folder("inst/shiny-examples", deploy_app)
