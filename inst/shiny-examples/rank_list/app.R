4273b7315e92275c6b584183fb047275bd817a88
Andrie de Vries <apdevries@gmail.com>
2020-06-17 21:14:19 +0100

    Add multi-drag example and swap example (#55)
    * Add multi-drag example #54
    
    * Fix `sortable_options()` to allow the swap plugin to work; add examples of swapping; modify shiny app to demonstrate basic, swap and multi-drag behaviour.
    
    * Include documentation update
    
    * Expand shiny app example with tab panel for default, multi-drag and swap behaviour.
    
    * Remove documentation link to `learnr::question_methods()` that no longer exists.
    
    * Moved `...` dots to first argument in sortable_options()
    
    * Updated NEWS with information about breaking change in the argument order for sortable_options()

--- inst/shiny-examples/rank_list/app.R
+++ inst/shiny-examples/rank_list/app.R
@@ -4,35 +4,82 @@
 library(shiny)
 library(sortable)
 
+labels <- list(
+  "one",
+  "two",
+  "three",
+  htmltools::tags$div(
+    htmltools::em("Complex"), " html tag without a name"
+  ),
+  "five" = htmltools::tags$div(
+    htmltools::em("Complex"), " html tag with name: 'five'"
+  )
+)
+
+rank_list_basic <- rank_list(
+  text = "Drag the items in any desired order",
+  labels = labels,
+  input_id = "rank_list_basic"
+)
+
+rank_list_swap <- rank_list(
+  text = "Notice that dragging causes items to swap",
+  labels = labels,
+  input_id = "rank_list_swap",
+  options = sortable_options(swap = TRUE)
+)
+
+rank_list_multi <- rank_list(
+  text = "You can select multiple items, then drag as a group",
+  labels = labels,
+  input_id = "rank_list_multi",
+  options = sortable_options(multiDrag = TRUE)
+)
+
+
+
 ui <- fluidPage(
   fluidRow(
     column(
       width = 12,
-      tags$b("Exercise"),
-      rank_list(
-        text = "Drag the items in any desired order",
-        labels = list(
-          "one",
-          "two",
-          "three",
-          htmltools::tags$div(
-            htmltools::em("Complex"), " html tag without a name"
-          ),
-          "five" = htmltools::tags$div(
-            htmltools::em("Complex"), " html tag with name: 'five'"
-          )
+    tags$h2("Default, multi-drag and swapping behaviour"),
+      tabsetPanel(
+        type = "tabs",
+        tabPanel(
+          "Default",
+            tags$b("Exercise"),
+            rank_list_basic,
+            tags$b("Result"),
+            verbatimTextOutput("results_basic")
         ),
-        input_id = "rank_list_1"
-      ),
-      tags$b("Result"),
-      verbatimTextOutput("results")
+        tabPanel(
+          "Multi-drag",
+            tags$b("Exercise"),
+            rank_list_multi,
+            tags$b("Result"),
+            verbatimTextOutput("results_multi")
+        ),
+        tabPanel(
+          "Swap",
+            tags$b("Exercise"),
+            rank_list_swap,
+            tags$b("Result"),
+            verbatimTextOutput("results_swap")
+        )
+      )
     )
   )
 )
 
 server <- function(input, output) {
-  output$results <- renderPrint({
-    input$rank_list_1 # This matches the input_id of the rank list
+  output$results_basic <- renderPrint({
+    input$rank_list_basic # This matches the input_id of the rank list
+  })
+  output$results_multi <- renderPrint({
+    input$rank_list_multi # This matches the input_id of the rank list
+  })
+  output$results_swap <- renderPrint({
+    input$rank_list_swap # This matches the input_id of the rank list
   })
 }
 
