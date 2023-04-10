5de5d68eabf2ef829f56aa0bcb050cd67ac296c7
Stephen Holsenbeck <stephen@virgalabs.io>
2023-04-10 13:37:32 -0400

    Create `setValue` methods for `rank/buckerlistBinding`s

--- inst/htmlwidgets/plugins/sortable-rstudio/rank_list_binding.js
+++ inst/htmlwidgets/plugins/sortable-rstudio/rank_list_binding.js
@@ -20,13 +20,28 @@ $.extend(ranklistBinding, {
   getValue: function(el) {
 
   },
+  setValue: function (el, data) {
+    if (data.header) {
+      $(el).find(".rank-list-title").text(data.text);
+    }
 
+    if (data.labels) {
+      let html = data.labels.map((v) => {
+        let el = document.createElement("div");
+        el.classList.add("rank-list-item");
+        el.innerHTML = v;
+        return el;
+      })
+      $('#' + data.id).html(html)
+      Shiny.setInputValue(data.id, data.labels, {priority: 'event'})
+    }
+
+  },
   subscribe: function(el, callback) {
 
   },
-
   receiveMessage: function(el, data) {
-    $(el).find(".rank-list-title").text(data.text);
+    this.setValue(el, data)
   }
 
 });
@@ -67,9 +82,24 @@ $.extend(bucketlistBinding, {
 
 
   },
+  setValue: function (el, data) {
+    if (data.header) {
+      $(el).find(".bucket-list-header").text(data.header);
+    }
 
+    if (data.labels) {
+      let html = data.labels.map((v) => {
+        let el = document.createElement("div");
+        el.classList.add("rank-list-item");
+        el.innerHTML = v;
+        return el;
+      })
+      $('#' + data.id).html(html)
+      Shiny.setInputValue(data.id, data.labels, {priority: 'event'})
+    }
+  },
   receiveMessage: function(el, data) {
-    $(el).find(".bucket-list-header").text(data.header);
+    this.setValue(el, data)
   }
 
 });
