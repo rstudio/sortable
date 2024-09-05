7c10fac0a0f71aaf7dbcbde96816f00b096bb330
andrie <apdevries@gmail.com>
2024-09-05 20:24:26 +0100

    Update SortableJS to version 1.15.3

--- scripts/download_sortablejs.R
+++ scripts/download_sortablejs.R
@@ -1,7 +1,13 @@
 
+get_new_sortable_version <- function() {
+  x <- readLines("https://unpkg.com/sortablejs", n = 1)
+  # sub("^.* Sortable (.*?) - MIT.*$", "\\1", x)
+  sub(".*(\\d+\\.\\d+\\.\\d+).*", "\\1", x)
+}
+get_new_sortable_version()
 
-sortable_version <- "1.15.0"
-yaml_version <- "1.15.0"
+sortable_version <- "1.15.3"
+yaml_version <- sortable_version
 
 
 writeLines(
@@ -13,3 +19,5 @@ sortable_yaml_file <- file.path("inst", "htmlwidgets", "sortable.yaml")
 config <- yaml::read_yaml(sortable_yaml_file)
 config$dependencies[[1]]$version <- yaml_version
 yaml::write_yaml(config, sortable_yaml_file)
+
+
