57fff619f79eebab4ecd799443deff7ea7f8e1c4
andrie <apdevries@gmail.com>
2025-12-07 13:20:19 +0000

    Fix failing tests

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -3,9 +3,14 @@ library(shinytest2)
 app_dir <- system.file("shiny/update_rank_list", package = "sortable")
 
 test_that("{shinytest2} recording: test_update_title", {
-  app <- AppDriver$new(variant = platform_variant(), name = "test_update_title",
-      app_dir =app_dir,
-      seed = 123, height = 945, width = 1619)
+  app <- AppDriver$new(
+    variant = platform_variant(),
+    name = "test_update_title",
+    app_dir = app_dir,
+    seed = 123,
+    height = 945,
+    width = 1619
+  )
   app$expect_screenshot()
   app$click("btnUpdateRank")
   app$expect_screenshot()
@@ -13,27 +18,45 @@ test_that("{shinytest2} recording: test_update_title", {
 
 
 test_that("{shinytest2} recording: test_change_labels", {
-  app <- AppDriver$new(name = "test_change_labels",
-      app_dir =app_dir,
-      seed = 123, height = 945, width = 1619)
+  app <- AppDriver$new(
+    name = "test_change_labels",
+    app_dir = app_dir,
+    seed = 123,
+    height = 945,
+    width = 1619
+  )
   app$expect_values(output = "results")
   app$click("btnChangeLabels")
-  app$set_inputs(rank_list_1 = c("W", "C", "R", "F", "I"), allow_no_input_binding_ = TRUE,
-      priority_ = "event")
+  app$set_inputs(
+    rank_list_1 = c("W", "C", "R", "F", "I"),
+    allow_no_input_binding_ = TRUE,
+    priority_ = "event"
+  )
   app$expect_values(output = "results")
 })
 
 
 test_that("{shinytest2} recording: test_empty_labels", {
-  app <- AppDriver$new(name = "test_empty_labels",
-      app_dir =app_dir,
-      seed = 123, height = 945, width = 1619)
+  app <- AppDriver$new(
+    name = "test_empty_labels",
+    app_dir = app_dir,
+    seed = 123,
+    height = 945,
+    width = 1619
+  )
   app$expect_values(output = "results")
   app$click("btnEmptyLabels")
-  app$set_inputs(rank_list_1 = character(0), allow_no_input_binding_ = TRUE, priority_ = "event")
+  app$set_inputs(
+    rank_list_1 = character(0),
+    allow_no_input_binding_ = TRUE,
+    priority_ = "event"
+  )
   app$expect_values(output = "results")
   app$click("btnChangeLabels")
-  app$set_inputs(rank_list_1 = c("M", "I", "Z", "W", "Q"), allow_no_input_binding_ = TRUE,
-      priority_ = "event")
+  app$set_inputs(
+    rank_list_1 = c("M", "I", "Z", "W", "Q"),
+    allow_no_input_binding_ = TRUE,
+    priority_ = "event"
+  )
   app$expect_values(output = "results")
 })
