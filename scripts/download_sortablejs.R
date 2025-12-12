2794cf00d78392cdfdb78fb0498a0ea5e12772a2
andrie <apdevries@gmail.com>
2025-12-12 20:08:28 +0000

    Update sortable.js to v1.15.6

--- scripts/download_sortablejs.R
+++ scripts/download_sortablejs.R
@@ -1,4 +1,3 @@
-
 get_new_sortable_version <- function() {
   x <- readLines("https://unpkg.com/sortablejs", n = 1)
   # sub("^.* Sortable (.*?) - MIT.*$", "\\1", x)
@@ -6,12 +5,15 @@ get_new_sortable_version <- function() {
 }
 get_new_sortable_version()
 
-sortable_version <- "1.15.3"
+sortable_version <- "1.15.6"
 yaml_version <- sortable_version
 
 
 writeLines(
-  readLines(paste0("https://unpkg.com/sortablejs@", sortable_version), warn = FALSE),
+  readLines(
+    paste0("https://unpkg.com/sortablejs@", sortable_version),
+    warn = FALSE
+  ),
   file.path("inst", "htmlwidgets", "lib", "sortable", "sortable.js")
 )
 
@@ -19,5 +21,3 @@ sortable_yaml_file <- file.path("inst", "htmlwidgets", "sortable.yaml")
 config <- yaml::read_yaml(sortable_yaml_file)
 config$dependencies[[1]]$version <- yaml_version
 yaml::write_yaml(config, sortable_yaml_file)
-
-
